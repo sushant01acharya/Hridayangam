1069031cb38a74f69a33a6c8d3c1c153d3fc06ae
Rk-Thakur <tranjan638@gmail.com>
2023-06-13 08:48:27 +0545

    Website

---
+++ src/components/services.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Our Services</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <img src={d.icon} alt="" height={100} />
+                  {/* <i className={d.icon}></i> */}
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
